eb386c6a2b48f3f4ddaf5d471369f7021bf700f2
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-02-19 14:45:16 -0300

    feat(main-card): create main card and add to 3 of them to home page
    - create MainCard component
    - add MainCard to homepage as 'most viewed today'

--- frontend/src/components/feed/home/Home.jsx
+++ frontend/src/components/feed/home/Home.jsx
@@ -1,9 +1,24 @@
+import MainCard from '../maincard/MainCard'
+
+
 export default function Home() {
+  // TODO: get most viewed essays and add to MainCard
 
   // render
   return (
     <div className="container">
-      <h1>Home page</h1>
+      <h1 className="pb-2 mb-4 fst-italic border-bottom">Most viewed today</h1>
+      <div className="row">
+        <div className="col-12 mb-3">
+          <MainCard category="U.S" title="BitCoin and Elon Musk" backgroundImage="/static/default-maincard-background.jpeg" url="/essay/bitcoin-and-elon-musk" />
+        </div>
+        <div className="col-12 col-md-6 mb-3">
+          <MainCard category="U.S" title="BitCoin and Elon Musk" backgroundImage="/static/default-maincard-background.jpeg" url="/essay/bitcoin-and-elon-musk" />
+        </div>
+        <div className="col-12 col-md-6 mb-3">
+          <MainCard category="U.S" title="BitCoin and Elon Musk" backgroundImage="/static/default-maincard-background.jpeg" url="/essay/bitcoin-and-elon-musk" />
+        </div>
+      </div>
     </div>
   )
 
---
+++ frontend/src/components/feed/maincard/MainCard.jsx
@@ -0,0 +1,26 @@
+import { Link, Switch } from 'react-router-dom'
+
+import './MainCard.css'
+
+
+export default function MainCard({ author, category, title, backgroundImage, url, premiumOnly }) {
+
+  //render
+  return (
+    <div className="main-card border rounded-3 shadow-sm overflow-hidden">
+      <Link className="clickable-card" to={url}>
+        <div className="background">
+          <img src={backgroundImage} alt="thumbnail" />
+        </div>
+        <div className="row p-3">
+          <div className="col-12">
+            <h5 className="category text-white fw-bold">{category}</h5>
+          </div>
+          <div className="title col-12 d-flex align-items-end">
+            <h3 className="text-white fw-bold">{title}</h3>
+          </div>
+        </div>
+      </Link>
+    </div>
+  )
+}
