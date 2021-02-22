aa9a5c524294cc70ced1e7b0e4b0e9d5e74b725b
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-02-22 12:04:01 -0300

    fix(main-card): remove 2 main cards in small devices

--- frontend/src/components/feed/home/Home.js
+++ frontend/src/components/feed/home/Home.js
@@ -13,10 +13,10 @@ export default function Home() {
         <div className="col-12 mb-3">
           <MainCard author="essayB" category="U.S" title="BitCoin and Elon Musk" backgroundImage="/static/default-maincard-background.jpeg" url="/essay/bitcoin-and-elon-musk" />
         </div>
-        <div className="col-12 col-md-6 mb-3">
+        <div className="col-12 col-md-6 mb-3 d-none d-md-block">
           <MainCard author="essayB" category="U.S" title="BitCoin and Elon Musk" backgroundImage="/static/default-maincard-background.jpeg" url="/essay/bitcoin-and-elon-musk" />
         </div>
-        <div className="col-12 col-md-6 mb-3">
+        <div className="col-12 col-md-6 mb-3 d-none d-md-block">
           <MainCard author="essayB" category="U.S" title="BitCoin and Elon Musk" backgroundImage="/static/default-maincard-background.jpeg" url="/essay/bitcoin-and-elon-musk" />
         </div>
 
