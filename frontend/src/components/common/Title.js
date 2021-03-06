f03ff1352f7b3283dbfb1418faa18a8cfecec1d4
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-03-06 20:38:19 -0300

    refactor(architecture): create base next architecture

--- frontend/src/components/common/Title.js
+++
@@ -1,6 +0,0 @@
-export default function Title({ children }) {
-  // render
-  return (
-    <h1 className="mb-4 fst-italic border-bottom">{children}</h1>
-  )
-}
--- frontend/src/components/user/LinkIcon.js
+++
@@ -1,7 +0,0 @@
-export default function LinkIcon({ href, iconName }) {
-  return (
-    <a target="_blank" style={{ color: 'initial' }} href={href}>
-      <i className={`fs-2 bi bi-${iconName}`}></i>
-    </a>
-  )
-}
