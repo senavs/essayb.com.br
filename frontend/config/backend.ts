7d28b57e037bca77be96556e52a74136675ec986
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-03-15 10:24:06 -0300

    fix(url): create client side env varaibles

--- frontend/config/backend.ts
+++ frontend/config/backend.ts
@@ -1,16 +1,16 @@
-// TODO: fix to work in server and client side
-const backendDomain = process.env.BACKEND_DOMAIN || '172.17.0.1:8080'
+const backendDomain = process.env.BACKEND_DOMAIN
+const protocol = process.env.USE_HTTPS ? 'https' : 'http'
 
 export const urls = {
   auth: {
-    login: process.env.URL_AUTH_LOGIN || `http://${backendDomain}/v1/auth/login`,
-    logout: process.env.URL_AUTH_LOGOUT || `http://${backendDomain}/v1/auth/logout`,
-    validate: process.env.URL_AUTH_VALIDATE || `http://${backendDomain}/v1/auth/validate`,
+    login: `${protocol}://${backendDomain}/v1/auth/login`,
+    logout: `${protocol}://${backendDomain}/v1/auth/logout`,
+    validate: `${protocol}://${backendDomain}/v1/auth/validate`,
   },
   user: {
-    search: process.env.URL_USER_SEARCH || `http://${backendDomain}/v1/users/{id_user_or_username}/search`,
-    profile_image: process.env.URL_USER_PROFILE_IMAGE || `http://${backendDomain}/v1/users/{username}/profile_image`,
-    create: process.env.URL_USER_CREATE || `http://${backendDomain}/v1/users/create`,
-    update: process.env.URL_USER_UPDATE || `http://${backendDomain}/v1/users/update`,
+    search: `${protocol}://${backendDomain}/v1/users/{id_user_or_username}/search`,
+    profile_image: `${protocol}://${backendDomain}/v1/users/{username}/profile_image`,
+    create: `${protocol}://${backendDomain}/v1/users/create`,
+    update: `${protocol}://${backendDomain}/v1/users/update`,
   }
 }
---
+++ frontend/next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  env: {
+    BACKEND_DOMAIN: process.env.BACKEND_DOMAIN,
+    USE_HTTPS: process.env.USE_HTTPS
+  },
+}
