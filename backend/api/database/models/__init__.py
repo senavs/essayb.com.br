934c7f99f5842a60cdeb44816f50f7ef9c071273
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-03-21 15:07:53 -0300

    feat(comment): create Coment table model

--- backend/api/database/models/__init__.py
+++ backend/api/database/models/__init__.py
@@ -1,6 +1,7 @@
 from .category import Category  # noqa
+from .comment import Comment  # noqa
+from .like import Like  # noqa
 from .post import Post  # noqa
 from .post_content import PostContent  # noqa
 from .token_blacklist import TokenBlacklist  # noqa
 from .user import User  # noqa
-from .like import Like  # noqa
---
+++ backend/api/database/models/comment.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, UniqueConstraint
+from sqlalchemy.orm import backref, relationship
+
+from .. import DeclarativeBase
+from .base import BaseModel
+
+
+class Comment(DeclarativeBase, BaseModel):
+    __tablename__ = 'COMMENT'
+
+    ID_COMMENT = Column(Integer, autoincrement=True, nullable=False, primary_key=True, unique=True)
+    ID_USER = Column(Integer, ForeignKey('USER.ID_USER', ondelete='CASCADE'), nullable=False, unique=False)
+    ID_POST = Column(Integer, ForeignKey('POST.ID_POST', ondelete='CASCADE'), nullable=False, unique=False)
+    COMMENT = Column(String(256), nullable=False, unique=False)
+
+    __table_args__ = (
+        UniqueConstraint('ID_USER', 'ID_POST'),
+    )
+
+    post = relationship('Post', backref=backref('likes', cascade='all,delete', lazy='dynamic'))
+    user = relationship('User', backref=backref('likes', cascade='all,delete', lazy='dynamic'))
