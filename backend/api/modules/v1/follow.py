b941507cd1e36133149b6f42a3d70be96c51e8e3
João Marcelo <joaomarcelo588@gmail.com>
2021-03-26 18:27:24 -0300

    Create archive follow.py

--- backend/api/modules/v1/follow.py
+++ backend/api/modules/v1/follow.py
@@ -1,1 +1,8 @@
-def check_follow (user_follower: str) -> bool:
+from ...database.client import DatabaseClient
+
+
+
+def check_follow (id_user_follower : int, id_user_following: int, connection: DatabaseClient = None) -> bool:
+    with DatabaseClient(connection=connection) as connection:
+
+
