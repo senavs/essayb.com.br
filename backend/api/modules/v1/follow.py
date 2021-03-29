6399f2ed2e08d26e62adb292b275fe86e4fa1ccf
João Marcelo <joaomarcelo588@gmail.com>
2021-03-29 12:48:11 -0300

    Created routes and modules by following

--- backend/api/modules/v1/follow.py
+++ backend/api/modules/v1/follow.py
@@ -31,3 +31,31 @@ def check_follower_by_id(id_user_follower: int, id_user_following: int, *, conne
 
     logger.info(f'Checking if username with id number {id_user_follower} is following {id_user_following} id')
     return bool(query)
+
+
+def check_following_by_username(username_following: str, username_follower: str, *, connection: DatabaseClient = None) -> bool:
+    """Check if some username is follower other one by username"""
+
+    logger.info(f'Checking if username {username_following} is follower {username_follower}')
+    with DatabaseClient(connection=connection) as connection:
+        follower = search_by_username(username_follower, connection=connection, raise_404=True, use_dict=False)
+        following = search_by_username(username_following, connection=connection, raise_404=True, use_dict=False)
+
+    query = connection.query(Follow).filter_by(ID_USER_FOLLOWING=following.ID_USER, ID_USER_FOLLOWER=follower.ID_USER.first())
+
+    logger.info(f'Checking if username {username_following} is follower {username_follower} successfully')
+    return bool(query)
+
+
+def check_following_by_id(id_user_following: int, id_user_follower: int, *, connection: DatabaseClient = None) -> bool:
+    """Check if some username is following other one by ID"""
+
+    logger.info(f'Checking if username with id number {id_user_following} is follower {id_user_follower} id')
+    with DatabaseClient(connection=connection) as connection:
+        follower = search_by_id(id_user_follower, connection=connection, raise_404=True, use_dict=False)
+        following = search_by_id(id_user_following, connection=connection, raise_404=True, use_dict=False)
+
+        query = connection.query(Follow).filter_by(ID_FOLLOWING = following.ID_USER, ID_USER_FOLLOWER=follower.ID_USER.first())
+
+    logger.info(f'Checking if username with id number {id_user_following} is follower {id_user_follower} id')
+    return bool(query)
--- backend/api/routes/v1/follow.py
+++ backend/api/routes/v1/follow.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from .models.follow import CheckFollowerResponse
-from ...modules.v1.follow import check_follower_by_username, check_follower_by_id
+from ...modules.v1.follow import check_follower_by_username, check_follower_by_id, check_following_by_id, check_following_by_username
 
 router = APIRouter(prefix='/follows', tags=['Follow'])
 
@@ -14,3 +14,14 @@ def _check_follower_username(username_or_id_user_follower: str, username_or_id_u
     else:
         is_following = check_follower_by_username(username_or_id_user_follower, username_or_id_user_following)
     return {'is_following': is_following}
+
+
+@router.get('/following/{username_or_id_user_following}/{username_or_id_user_follower}/check', summary='Check following by ID or username', status_code=200,
+            response_model=CheckFollowerResponse)
+def _check_following_username(username_or_id_user_following: str, username_or_id_user_follower: str):
+    if username_or_id_user_following.isdigit() and username_or_id_user_follower.isdigit():
+        is_follower = check_following_by_id(int(username_or_id_user_following), int(username_or_id_user_follower))
+    else:
+        is_follower = check_following_by_username(username_or_id_user_following, username_or_id_user_follower)
+    return {'is_following': is_follower}
+
