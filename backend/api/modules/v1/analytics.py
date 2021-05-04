82936f16547a52a969fa11e8f8fa88eef2a765c0
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-05-04 18:49:42 -0300

    feat(analytics): create most_liked_posts routes and modules

--- backend/api/modules/v1/analytics.py
+++ backend/api/modules/v1/analytics.py
@@ -2,7 +2,7 @@ from loguru import logger
 from sqlalchemy import func, text
 
 from ...database.client import DatabaseClient
-from ...database.models import Follow, User
+from ...database.models import Follow, User, Post, Like
 
 
 def most_followed_users(top: int, *, connection: DatabaseClient = None) -> list[dict]:
@@ -21,4 +21,23 @@ def most_followed_users(top: int, *, connection: DatabaseClient = None) -> list[
         result = [user.to_dict(exclude=['PROFILE_IMAGE', 'PASSWORD']) for user in users.all()]
 
     logger.info(f'Listed top {top} most followed users successfully')
+    return result
+
+
+def most_liked_posts(top: int, *, connection: DatabaseClient = None) -> list[dict]:
+    """List top most followed users"""
+
+    logger.info(f'List top {top} most liked posts')
+    with DatabaseClient(connection=connection) as conn:
+        likes_sub = conn.query(Like.ID_POST, func.count(Like.ID_POST).label('total')) \
+            .group_by(Like.ID_POST) \
+            .order_by(text('total DESC')) \
+            .subquery('likes')
+        posts = conn.query(Post) \
+            .join(likes_sub, Post.ID_POST == likes_sub.c.ID_POST) \
+            .limit(top)
+
+        result = [post.to_dict(exclude=['THUMBNAIL']) for post in posts.all()]
+
+    logger.info(f'Listed top {top} most liked posts successfully')
     return result
--- backend/api/routes/v1/analytics.py
+++ backend/api/routes/v1/analytics.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Query
 
-from ...modules.v1.analytics import most_followed_users
-from .models.analytics import ListMostFollowedUsers
+from ...modules.v1.analytics import most_followed_users, most_liked_posts
+from .models.analytics import ListMostFollowedUsers, ListMostLikedPosts
 
 router = APIRouter(prefix='/analytics', tags=['Analytics'])
 
@@ -9,3 +9,8 @@ router = APIRouter(prefix='/analytics', tags=['Analytics'])
 @router.get('/most_followed_users', summary='List top most followed users', status_code=200, response_model=ListMostFollowedUsers)
 def _most_followed_users(top: int = Query(10, ge=0)):
     return most_followed_users(top)
+
+
+@router.get('/most_liked_posts', summary='List top most liked posts', status_code=200, response_model=ListMostLikedPosts)
+def _most_liked_posts(top: int = Query(10, ge=0)):
+    return most_liked_posts(top)
