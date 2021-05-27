274144267c863332535fb31f4044466320ad3a0e
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-05-27 20:19:21 -0300

    feat(homepage): add last posts

--- frontend/src/libs/services/analytics.ts
+++ frontend/src/libs/services/analytics.ts
@@ -5,8 +5,12 @@ import { PostInterface } from "./post";
 
 
 export type MostFollowedUsersInterface = Array<UserInterface>
+
 export type MostPostLikedUserPremium = Array<PostInterface>
+
 export type DiscoveryPosts = Array<PostInterface>
+
+export type LastPosts = Array<PostInterface>
 
 export default class AnalyticsService {
   static async mostFollowedUsers(): Promise<MostFollowedUsersInterface> {
@@ -19,5 +23,9 @@ export default class AnalyticsService {
 
   static async discoveryPosts(top: number = 10): Promise<DiscoveryPosts> {
     return await callAPI(urls.analytics.discovery + `?top=${top}`, 'GET')
+  }
+
+  static async lastPosts(skip: number = 0, limit: number = 10): Promise<LastPosts> {
+    return await callAPI(urls.analytics.lastPosts + `?skip=${skip}&limit=${limit}`, 'GET')
   }
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,10 +6,11 @@ import Layout from "../components/common/Layout";
 import Carousel from "../components/homepage/Carousel";
 import CardPost from "../components/homepage/CardPost";
 import DiscoveryCard from "../components/homepage/Discovery"
-import AnalyticsService, { DiscoveryPosts, MostFollowedUsersInterface, MostPostLikedUserPremium } from "../libs/services/analytics";
+import AnalyticsService, { DiscoveryPosts, MostFollowedUsersInterface, MostPostLikedUserPremium, LastPosts } from "../libs/services/analytics";
 import { AuthenticationData, getAuthenticationData } from "../libs/props/auth";
 import { CategoryData, getCategoryData } from "../libs/props/category";
 import About from "src/components/homepage/About";
+import { useState } from "react";
 
 
 interface IndexProps {
@@ -18,9 +19,23 @@ interface IndexProps {
   topUsers: MostFollowedUsersInterface
   topPostMonthly: MostPostLikedUserPremium
   discovery: DiscoveryPosts
+  lastPosts: LastPosts
 }
 
-export default function Index({ authenticationData, categoryData, topUsers, topPostMonthly, discovery }: IndexProps) {
+export default function Index({ authenticationData, categoryData, topUsers, topPostMonthly, discovery, lastPosts }: IndexProps) {
+  const [skip, setSkip] = useState(10)
+  const [posts, setPosts] = useState(lastPosts)
+
+  function onClickLoadMore(event) {
+    AnalyticsService.lastPosts(skip)
+      .then((res) => {
+        if (res.length < 10) {
+          event.target.hidden = true
+        }
+        setSkip(skip + 10)
+        setPosts(posts.concat(res))
+      })
+  }
 
   return (
 
@@ -52,14 +67,31 @@ export default function Index({ authenticationData, categoryData, topUsers, topP
         {/* post list and users */}
         <div className="row">
           <div className="col-12 col-md-8">
-            <div className="row">
-              <div className="col">
-                <Title>Last posts</Title>
-              </div>
+            <div className="row mb-5">
+              <Title>Last posts</Title>
+
+              {posts.map((e, i) => {
+                return (<div className="col-md-6" key={i}>
+                  <DiscoveryCard
+                    id_post={e.id_post}
+                    title={e.title}
+                    descriprion={e.description}
+                    created_at={e.created_at}
+                  />
+                </div>)
+              })}
+
+              {/* button loadmore */}
+              {posts.length >= 10 && (
+                <div className="d-flex justify-content-center mt-3">
+                  <button className="btn btn-outline-secondary" onClick={onClickLoadMore}>Load more</button>
+                </div>
+              )}
             </div>
 
-            <div className="row">
+            <div className="row mb-5">
               <Title>Discovery</Title>
+
               {discovery.map((e, i) => {
                 return (<div className="col-md-6" key={i}>
                   <DiscoveryCard
@@ -109,10 +141,11 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const categoryData = await getCategoryData()
 
   const topUsers = await AnalyticsService.mostFollowedUsers()
+  const lastPosts = await AnalyticsService.lastPosts()
   const topPostMonthly = await AnalyticsService.mostLikedPostUserPremium(2)
-  const discovery = await AnalyticsService.discoveryPosts(6)
+  const discovery = await AnalyticsService.discoveryPosts(4)
 
   return {
-    props: { authenticationData, categoryData, topUsers, topPostMonthly, discovery }
+    props: { authenticationData, categoryData, topUsers, topPostMonthly, discovery, lastPosts }
   }
 }
